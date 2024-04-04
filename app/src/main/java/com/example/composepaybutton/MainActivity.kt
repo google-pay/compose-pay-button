034a9648c66971f5bc14c0e31a396e0a75d99de6
Stefan M <StefMa@users.noreply.github.com>
2024-04-04 07:13:55 +0000

    Add Gradle version catalog

--- app/src/main/java/com/example/composepaybutton/MainActivity.kt
+++ app/src/main/java/com/example/composepaybutton/MainActivity.kt
@@ -23,16 +23,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Divider
-import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.PlatformTextStyle
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
 import com.google.pay.button.ButtonTheme
